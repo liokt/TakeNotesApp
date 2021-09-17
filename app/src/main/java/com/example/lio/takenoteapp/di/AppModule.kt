e72b7611be669a8c9678b2aa98b10a0fbe019608
Lio <liomazza@gmail.com>
2021-09-17 08:18:20 -0300

    dagger hilt setup

---
+++ app/src/main/java/com/example/lio/takenoteapp/di/AppModule.kt
@@ -0,0 +1,75 @@
+package com.example.lio.takenoteapp.di
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.room.Room
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import com.example.lio.takenoteapp.data.local.NotesDatabase
+import com.example.lio.takenoteapp.data.remote.BasicAuthInterceptor
+import com.example.lio.takenoteapp.data.remote.NoteApi
+import com.example.lio.takenoteapp.other.Constants.BASE_URL
+import com.example.lio.takenoteapp.other.Constants.DATABASE_NAME
+import com.example.lio.takenoteapp.other.Constants.ENCRYPTED_SHARED_PREF_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideNotesDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(context, NotesDatabase::class.java, DATABASE_NAME).build()
+
+    @Singleton
+    @Provides
+    fun provideNoteDao(db: NotesDatabase) = db.noteDao()
+
+    @Singleton
+    @Provides
+    fun provideBasicAuthInterceptor() = BasicAuthInterceptor()
+
+    @Singleton
+    @Provides
+    fun providesNoteApi(
+        basicAuthInterceptor: BasicAuthInterceptor
+    ): NoteApi {
+        val client = OkHttpClient.Builder()
+            .addInterceptor(basicAuthInterceptor)
+            .build()
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+            .create(NoteApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideEncryptedSharedPreferences(
+        @ApplicationContext context: Context
+    ): SharedPreferences {
+        val masterKey = MasterKey.Builder(context)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+        return EncryptedSharedPreferences.create(
+            context,
+            ENCRYPTED_SHARED_PREF_NAME,
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
+}
--- app/src/main/java/com/example/lio/takenoteapp/other/Constants.kt
+++ app/src/main/java/com/example/lio/takenoteapp/other/Constants.kt
@@ -3,4 +3,7 @@ package com.example.lio.takenoteapp.other
 object Constants {
 
     val IGNORE_AUTH_URLS = listOf("/login", "/register")
+    const val DATABASE_NAME = "notes_db"
+    const val BASE_URL = "http://10.0.2.2:8080"
+    const val ENCRYPTED_SHARED_PREF_NAME = "enc_shared_pref"
 }
