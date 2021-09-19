146156537274c5cb931c04711052337a437e2675
Lio <liomazza@gmail.com>
2021-09-19 11:24:45 -0300

    notes adapter

--- app/src/main/java/com/example/lio/takenoteapp/ui/notes/NoteViewModel.kt
+++ app/src/main/java/com/example/lio/takenoteapp/ui/notes/NoteViewModel.kt
@@ -1,6 +1,25 @@
 package com.example.lio.takenoteapp.ui.notes
 
-import androidx.lifecycle.ViewModel
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.*
+import com.example.lio.takenoteapp.data.local.entities.Note
+import com.example.lio.takenoteapp.other.Event
+import com.example.lio.takenoteapp.other.Resource
+import com.example.lio.takenoteapp.repositories.NoteRepository
 
-class NoteViewModel: ViewModel() {
+class NoteViewModel @ViewModelInject constructor(
+    private val repository: NoteRepository
+): ViewModel() {
+
+    private val _forceUpdate = MutableLiveData<Boolean>(false)
+
+    private val _allNotes = _forceUpdate.switchMap {
+        repository.getAllNotes().asLiveData(viewModelScope.coroutineContext)
+    }.switchMap {
+        MutableLiveData(Event(it))
+    }
+    val allNotes: LiveData<Event<Resource<List<Note>>>> = _allNotes
+
+    fun syncAllNotes() = _forceUpdate.postValue(true)
+
 }
--- app/src/main/java/com/example/lio/takenoteapp/ui/notes/NotesFragment.kt
+++ app/src/main/java/com/example/lio/takenoteapp/ui/notes/NotesFragment.kt
@@ -1,30 +1,53 @@
 package com.example.lio.takenoteapp.ui.notes
 
 import android.content.SharedPreferences
+import android.content.pm.ActivityInfo.SCREEN_ORIENTATION_USER
 import android.os.Bundle
 import android.view.*
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.lio.takenoteapp.R
+import com.example.lio.takenoteapp.adapters.NoteAdapter
 import com.example.lio.takenoteapp.other.Constants.KEY_LOGGED_IN_EMAIL
 import com.example.lio.takenoteapp.other.Constants.KEY_PASSWORD
 import com.example.lio.takenoteapp.other.Constants.NO_EMAIL
 import com.example.lio.takenoteapp.other.Constants.NO_PASSWORD
+import com.example.lio.takenoteapp.other.Status
 import com.example.lio.takenoteapp.ui.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_notes.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class NotesFragment: BaseFragment(R.layout.fragment_notes) {
+class NotesFragment : BaseFragment(R.layout.fragment_notes) {
 
     @Inject
     lateinit var sharedPref: SharedPreferences
 
+    private val viewModel: NoteViewModel by viewModels()
+
+    private lateinit var noteAdapter: NoteAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        requireActivity().requestedOrientation = SCREEN_ORIENTATION_USER
+        setupRecyclerView()
+        subscribeToObservers()
+
+        noteAdapter.setOnItemClickListener {
+            findNavController().navigate(
+                NotesFragmentDirections.actionNotesFragmentToNoteDetailFragment(it.id)
+            )
+        }
         fabAddNote.setOnClickListener {
-            findNavController().navigate(NotesFragmentDirections.actionNotesFragmentToAddEditNoteFragment(""))
+            findNavController().navigate(
+                NotesFragmentDirections.actionNotesFragmentToAddEditNoteFragment(
+                    ""
+                )
+            )
         }
     }
 
@@ -35,6 +58,43 @@ class NotesFragment: BaseFragment(R.layout.fragment_notes) {
     ): View? {
         setHasOptionsMenu(true)
         return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    private fun setupRecyclerView() = rvNotes.apply {
+        noteAdapter = NoteAdapter()
+        adapter = noteAdapter
+        layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.allNotes.observe(viewLifecycleOwner, Observer {
+            it?.let { event ->
+                val result = event.peekContent()
+                when (result.status) {
+                    Status.SUCCESS -> {
+                        noteAdapter.notes = result.data!!
+                        swipeRefreshLayout.isRefreshing = false
+                    }
+                    Status.ERROR -> {
+                        event.getContentIfNotHandled()?.let { errorResource ->
+                            errorResource.message?.let { message ->
+                                showSnackbar(message)
+                            }
+                        }
+                        result.data?.let{ notes ->
+                            noteAdapter.notes = notes
+                        }
+                        swipeRefreshLayout.isRefreshing = false
+                    }
+                    Status.LOADING -> {
+                        result.data?.let { notes ->
+                            noteAdapter.notes = notes
+                        }
+                        swipeRefreshLayout.isRefreshing = true
+                    }
+                }
+            }
+        })
     }
 
     private fun logout() {
@@ -51,9 +111,9 @@ class NotesFragment: BaseFragment(R.layout.fragment_notes) {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-       when(item.itemId) {
-           R.id.miLogout -> logout()
-       }
+        when (item.itemId) {
+            R.id.miLogout -> logout()
+        }
         return super.onOptionsItemSelected(item)
     }
 
